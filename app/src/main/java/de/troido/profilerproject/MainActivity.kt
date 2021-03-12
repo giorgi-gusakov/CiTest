0e94a83af150a8b5a350d4d78afcb548260d351a
Giorgi Gusakov <giorgi@simvelop.de>
2021-03-12 11:49:18 +0100

    initial project

---
+++ app/src/main/java/de/troido/profilerproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.troido.profilerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
